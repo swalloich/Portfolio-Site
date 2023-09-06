93f08eb1d826cf6cfe6870693ee57eb0b1431bf4
swalloich <nels.jac42@gmail.com>
2023-09-05 23:05:07 -0600

    migrated portfolio to gh-pages version

---
+++ src/components/header.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { NavLink, Route, Routes, BrowserRouter } from 'react-router-dom';
+
+import { Home } from '../pages/home';
+import { About } from '../pages/about';
+import { Contact } from '../pages/contact';
+
+import "../styles/utility-classes.css";
+import "../styles/components/header.css";
+import { Projects } from '../pages/projects';
+
+export class Header extends React.Component {
+    render () {
+        return (
+            <>
+                <BrowserRouter>
+                    <header className='header container box-shadow-bottom'>
+                        <div className='logo'>
+                            <h1>Logo Here</h1>
+                        </div>
+                        <nav className='main-nav'>
+                            <ul>
+                                <li>
+                                    <NavLink to='' activeClassName="active">
+                                        Home
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to='about' activeClassName="active">
+                                        About
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to='projects' activeClassName="active">
+                                        Projects
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to='contact' activeClassName="active">
+                                        Contact
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        </nav>
+                    </header>
+
+                    <Routes>
+                        <Route path='/' element={ <Home /> } />
+                        <Route path='/about' element={ <About /> } />
+                        <Route path='/projects' element={ <Projects /> } />
+                        <Route path='/contact' element={ <Contact /> } />
+                    </Routes>
+                </BrowserRouter>
+            </>
+        );
+    }
+}
---
+++ src/pages/contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class Contact extends React.Component {
+    render () {
+        return (
+            <>
+                <main>
+                    <h1>Contact Page</h1>
+                </main>
+            </>
+        )
+    }
+}
