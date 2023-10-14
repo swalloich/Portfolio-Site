685a60f141b580a83045bf8635df6950048c566e
swalloich <nels.jac42@gmail.com>
2023-10-14 12:43:05 -0600

    added fade-in to about page.

---
+++ src/components/fade-in.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import "../styles/utilities/animations.css"
+
+export class FadeIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mutableClasses: ["jn-opacity-0"],
+        }
+    }
+
+    #removeClass(className) {
+        let classArray = this.state.mutableClasses;
+        for (let i = 0; i < classArray.length; i++)
+        {
+            if (classArray[i] === className) {
+                classArray.splice(i, 1);
+                this.setState({ mutableClasses: classArray });
+            }
+        }
+    }
+
+    #addClass(className) {
+        this.state.mutableClasses.push(className);
+    }
+
+    get classList() {
+        let list = 'jn-anim-fade-in';
+        list += ` ${this.state.mutableClasses.join(' ')}`;
+        list += ` ${this.props.classList}`;
+        return list;
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.#addClass('jn-opacity-100');
+            this.#removeClass('jn-opacity-0');
+        }, 1000);
+    }
+
+    render() {
+        return (
+            <div className={ this.classList }>
+                { this.props.children }
+            </div>
+        );
+    }
+}
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,20 +1,26 @@
 import React, { lazy } from 'react';
 
+import { FadeIn } from "../components/fade-in"
+
 import headshot from "../images/jacob_headshot.jpg"
+
+import "../styles/utilities/animations.css"
 
 export class About extends React.Component {
     render () {
         return (
             <>
-                <main className='d-flex flex-column align-items-center w-100'>
-                    <div class="w-100 d-flex justify-content-end py-5">
-                        <div class="right-edge-card jn-bg-super-light-blue-t">
+                <main className='d-flex flex-column align-items-center w-100 overflow-hidden'>
+                    <div className="w-100 d-flex justify-content-end py-5 jn-anim-slide-from-right">
+                        <div className="right-edge-card jn-bg-super-light-blue-t">
                             <img src={headshot} alt="" height={300} width={300} onLoadStart={lazy}/>
-                            <h1 className='jn-font-xl-md m-0 ms-5'>Jacob Nelson</h1>
+                            <h1 className='jn-font-xl-md m-0 ms-3 ms-md-5'>Jacob Nelson</h1>
                         </div>
                     </div>
                     <div className="jn-container">
-                        <h1>About Page</h1>
+                        <FadeIn>
+                            <div className='jn-font-xl'>Need a website?</div>
+                        </FadeIn>
                     </div>
                 </main>
             </>
