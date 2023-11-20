8d143cf68b85190cc5d09ed76a97e1262deb59ed
swalloich <nels.jac42@gmail.com>
2023-11-20 14:10:58 -0700

    fixed links on projects and contact page.

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -13,35 +13,33 @@ import "bootstrap/dist/css/bootstrap-grid.min.css";
 export class Contact extends React.Component {
     render () {
         return (
-            <>
-                <main className='d-flex justify-content-center'>
-                    <div className='cointainer-fluid'>
-                        <div className='row flex-column align-items-center flex-lg-row h-100'>
-                            <div className='col-10 col-md-6 col-lg-8 d-flex justify-content-end align-items-center mt-5 mt-lg-0 text-end jn-separator-300-end'>
-                                <h1 className='jn-font-l jn-font-xl-lg pe-5'>I can help bring your company's vision to life.</h1>
-                            </div>
-                            <div className='col-10 col-md-6 col-lg-4 d-flex mt-3 mt-lg-0 align-items-center jn-separator-300-start jn-separator-lg-none'>
-                                <div class="ps-5 ps-lg-3">
-                                    <h2 className='mt-0'>Find me at:</h2>
-                                    <a href="mailto:nels.jac42@gmail.com">nels.jac42@gmail.com</a>
-                                    <div className='d-flex align-items-center mt-3'>
-                                        <a className='jn-font-linkedin-blue' href='https://linkedin.com/in/nelsjac42' target='_blank' rel='noreferrer'>
-                                            <FontAwesomeIcon className='jn-font-axl' icon={icon({name: 'linkedin', style: 'brands'})} />
-                                        </a>
-                                        <div className='ms-3'>Linkedin</div>
-                                    </div>
-                                    <div className='d-flex align-items-center mt-3'>
-                                        <a className='jn-font-github-logo-color d-flex align-items-center'>
-                                            <FontAwesomeIcon className='jn-font-axl' icon={icon({name: 'github', style: 'brands'})} />
-                                        </a>
-                                        <div className='ms-3'>GitHub</div>
-                                    </div>
+            <main className='d-flex justify-content-center'>
+                <div className='cointainer-fluid'>
+                    <div className='row flex-column align-items-center flex-lg-row h-100'>
+                        <div className='col-10 col-md-6 col-lg-8 d-flex justify-content-end align-items-center mt-5 mt-lg-0 text-end jn-separator-300-end'>
+                            <h1 className='jn-font-l jn-font-xl-lg pe-5'>I can help bring your company's vision to life.</h1>
+                        </div>
+                        <div className='col-10 col-md-6 col-lg-4 d-flex mt-3 mt-lg-0 align-items-center jn-separator-300-start jn-separator-lg-none'>
+                            <div className="ps-5 ps-lg-3">
+                                <h2 className='mt-0'>Find me at:</h2>
+                                <a href="mailto:nels.jac42@gmail.com">nels.jac42@gmail.com</a>
+                                <div className='d-flex align-items-center mt-3'>
+                                    <a className='d-flex align-items-center' href='https://linkedin.com/in/nelsjac42' target='_blank' rel='noreferrer'>
+                                        <FontAwesomeIcon className='jn-font-axl jn-font-linkedin-blue' icon={icon({name: 'linkedin', style: 'brands'})} />
+                                        <div className='ms-3 text-white'>Linkedin</div>
+                                    </a>
+                                </div>
+                                <div className='d-flex align-items-center mt-3'>
+                                    <a className='d-flex align-items-center' href="https://github.com/swalloich" target="_blank" rel="noreferrer">
+                                        <FontAwesomeIcon className='jn-font-axl jn-font-github-logo-color' icon={icon({name: 'github', style: 'brands'})} />
+                                        <div className='ms-3 text-white'>GitHub</div>
+                                    </a>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </main>
-            </>
+                </div>
+            </main>
         )
     }
 }
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -9,129 +9,127 @@ import "bootstrap/dist/css/bootstrap-grid.min.css";
 export class Projects extends React.Component {
     render() {
         return (
-            <>
-                <main className='d-flex justify-content-center'>
-                    <div className='jn-container'>
-                        <h1>Projects</h1>
-                        <div className='gutter-container flex-gap-md flex-direction-column flex-direction-md-row'>
-                            <div className='full-card half-card-md'>
-                                <h2 className='mb-0'>This Site</h2>
-                                <div className='card-body'>
-                                    <h4 className='mb-0'>Description</h4>
-                                    <p className='mt-0'>
-                                        This website was designed, built, and deployed by me.
-                                        Bootstrap was used to make building a site that is
-                                        responsive easier. React was used to build it, so that I
-                                        could take advantage of templating. This site is hosted on
-                                        an Oracle Cloud always free teir instance that is managed
-                                        by me as well.
-                                        
-                                    </p>
-                                </div>
-                                <h4>Skills & Technologies Used</h4>
-                                <div className='gutter-container flex-gap-xsm'>
-                                    <Tag>React</Tag>
-                                    <Tag>CSS</Tag>
-                                    <Tag>Bootstrap 5</Tag>
-                                    <Tag>JavaScript</Tag>
-                                    <Tag>Oracle Cloud</Tag>
-                                    <Tag>Git</Tag>
-                                    <Tag>Responsive Design</Tag>
-                                </div>
+            <main className='d-flex justify-content-center'>
+                <div className='jn-container'>
+                    <h1 className='pt-5 pb-4'>Projects</h1>
+                    <div className='gutter-container flex-gap-md flex-direction-column flex-direction-md-row'>
+                        <div className='full-card half-card-md'>
+                            <h2 className='mb-0'>This Site</h2>
+                            <div className='card-body'>
+                                <h4 className='mb-0'>Description</h4>
+                                <p className='mt-0'>
+                                    This website was designed, built, and deployed by me.
+                                    Bootstrap was used to make building a site that is
+                                    responsive easier. React was used to build it, so that I
+                                    could take advantage of templating. This site is hosted on
+                                    an Oracle Cloud always free teir instance that is managed
+                                    by me as well.
+
+                                </p>
                             </div>
-                            <div className='full-card half-card-md'>
-                                <h2 className='mb-0'>CSS Cleanup</h2>
-                                <div className='card-body'>
-                                    <h4 className='mb-0'>Source</h4>
-                                    <a className='jn-font-pop-blue mt-1' href="https://education.byu.edu/" target='_blank' rel='noreferrer'>education.byu.edu</a>
-                                    <h4 className='mb-0'>Description</h4>
-                                    <p className='mt-0'>
-                                        Our team determined that our site was suffering in two ways
-                                        because of the way out style sheets were structured. These were:
-                                    </p>
-                                    <ul className='ms-5'>
-                                        <li>Decreased page load times</li>
-                                        <li>Greater difficulty training new employees</li>
-                                    </ul>
-                                    <p>
-                                        We decided that using sass in our future stylesheets would
-                                        remedy both these issues. <br />After the new structure was
-                                        decided on, along with a set of rules team members would
-                                        be expected to follow going forward, we set our plan into
-                                        motion. The result was page load times approximately 3x
-                                        faster than before.
-                                    </p>
-                                </div>
-                                <h4 className='mb-2'>Skills & Technologies Used</h4>
-                                <div className='gutter-container flex-gap-xsm'>
-                                    <Tag>SASS</Tag>
-                                    <Tag>node.js</Tag>
-                                    <Tag>CSS</Tag>
-                                    <Tag>PHP</Tag>
-                                    <Tag>Bootstrap 5</Tag>
-                                    <Tag>Drupal</Tag>
-                                    <Tag>Git</Tag>
-                                    <Tag>Twig</Tag>
-                                </div>
+                            <h4>Skills & Technologies Used</h4>
+                            <div className='gutter-container flex-gap-xsm'>
+                                <Tag>React</Tag>
+                                <Tag>CSS</Tag>
+                                <Tag>Bootstrap 5</Tag>
+                                <Tag>JavaScript</Tag>
+                                <Tag>Oracle Cloud</Tag>
+                                <Tag>Git</Tag>
+                                <Tag>Responsive Design</Tag>
                             </div>
-                            <div className='full-card half-card-md'>
-                                <h2 >McKay School Directory</h2>
-                                <div className='card-body'>
-                                    <h4 className='my-0'>Source</h4>
-                                    <a className='jn-font-pop-blue mt-1' href="https://education.byu.edu/directory" target='_blank' rel='noreferrer'>McKay School Directory</a>
-                                    <h4 className='mb-0'>Description</h4>
-                                    <p className='mt-0'>
-                                        The site's directory page worked fine on desktop, but was
-                                        near impossible to use on mobile devices. The original was
-                                        a table automatically generated by Drupal. I crafted a new
-                                        directory page using divs and Bootstrap that responded to
-                                        view width, making it usable both on desktop and mobile.
-                                    </p>
-                                </div>
-                                <h4 className='mb-2'>Skills and Technologies Used</h4>
-                                <div className='gutter-container flex-gap-xsm'>
-                                    <Tag>Bootstrap 5</Tag>
-                                    <Tag>Drupal</Tag>
-                                    <Tag>Twig</Tag>
-                                    <Tag>Responsive Design</Tag>
-                                </div>
+                        </div>
+                        <div className='full-card half-card-md'>
+                            <h2 className='mb-0'>CSS Cleanup</h2>
+                            <div className='card-body'>
+                                <h4 className='mb-0'>Source</h4>
+                                <a className='jn-font-pop-blue mt-1' href="https://education.byu.edu/" target='_blank' rel='noreferrer'>education.byu.edu</a>
+                                <h4 className='mb-0'>Description</h4>
+                                <p className='mt-0'>
+                                    Our team determined that our site was suffering in two ways
+                                    because of the way out style sheets were structured. These were:
+                                </p>
+                                <ul className='ms-5'>
+                                    <li>Decreased page load times</li>
+                                    <li>Greater difficulty training new employees</li>
+                                </ul>
+                                <p>
+                                    We decided that using sass in our future stylesheets would
+                                    remedy both these issues. <br />After the new structure was
+                                    decided on, along with a set of rules team members would
+                                    be expected to follow going forward, we set our plan into
+                                    motion. The result was page load times approximately 3x
+                                    faster than before.
+                                </p>
                             </div>
-                            <div className='full-card half-card-md'>
-                                <h2>Speech & Language Clinic</h2>
-                                <div className='card-body'>
-                                    <h4 className='my-0'>Source</h4>
-                                    <a className='jn-font-pop-blue mt-1 d-block' href='https://education.byu.edu/speech-and-language-clinic' target='_blank' rel='noreferrer'>Speech & Language Clinic</a>
-                                    <ul className='mt-0'>
-                                        <li>
-                                            <a className='jn-font-pop-blue ms-3 d-block' href="https://education.byu.edu/speech-and-language-clinic/apply" target='_blank' rel='noreferrer'>/apply</a>
-                                        </li>
-                                        <li>
-                                            <a className='jn-font-pop-blue ms-3 d-block' href="https://education.byu.edu/speech-and-language-clinic/about" target='_blank' rel='noreferrer'>/about</a>
-                                        </li>
-                                        <li>
-                                            <a className='jn-font-pop-blue ms-3 d-block' href='https://education.byu.edu/speech-and-language-clinic/resources'>/resources</a>
-                                        </li>
-                                    </ul>
-                                    <h4 className='mb-0'>Description</h4>
-                                    <p className='mt-0'>
-                                        I Constructed a series of pages from screenshots provided
-                                        by our UX team. I implemented responsive design techniques
-                                        to ensure an optimal experience on both desktop and mobile
-                                        platforms.
-                                    </p>
-                                </div>
-                                <h4 className='mb-2'>Skills and Technologies Used</h4>
-                                <div className='gutter-container flex-gap-xsm'>
-                                    <Tag>Bootstrap 5</Tag>
-                                    <Tag>CSS</Tag>
-                                    <Tag>HTML 5</Tag>
-                                    <Tag>Responsive Design</Tag>
-                                </div>
+                            <h4 className='mb-2'>Skills & Technologies Used</h4>
+                            <div className='gutter-container flex-gap-xsm'>
+                                <Tag>SASS</Tag>
+                                <Tag>node.js</Tag>
+                                <Tag>CSS</Tag>
+                                <Tag>PHP</Tag>
+                                <Tag>Bootstrap 5</Tag>
+                                <Tag>Drupal</Tag>
+                                <Tag>Git</Tag>
+                                <Tag>Twig</Tag>
+                            </div>
+                        </div>
+                        <div className='full-card half-card-md'>
+                            <h2 >McKay School Directory</h2>
+                            <div className='card-body'>
+                                <h4 className='my-0'>Source</h4>
+                                <a className='jn-font-pop-blue mt-1' href="https://education.byu.edu/directory" target='_blank' rel='noreferrer'>McKay School Directory</a>
+                                <h4 className='mb-0'>Description</h4>
+                                <p className='mt-0'>
+                                    The site's directory page worked fine on desktop, but was
+                                    near impossible to use on mobile devices. The original was
+                                    a table automatically generated by Drupal. I crafted a new
+                                    directory page using divs and Bootstrap that responded to
+                                    view width, making it usable both on desktop and mobile.
+                                </p>
+                            </div>
+                            <h4 className='mb-2'>Skills and Technologies Used</h4>
+                            <div className='gutter-container flex-gap-xsm'>
+                                <Tag>Bootstrap 5</Tag>
+                                <Tag>Drupal</Tag>
+                                <Tag>Twig</Tag>
+                                <Tag>Responsive Design</Tag>
+                            </div>
+                        </div>
+                        <div className='full-card half-card-md'>
+                            <h2>Speech & Language Clinic</h2>
+                            <div className='card-body'>
+                                <h4 className='my-0'>Source</h4>
+                                <a className='jn-font-pop-blue mt-1 d-block' href='https://education.byu.edu/comd/speech-and-language-clinic' target='_blank' rel='noreferrer'>Speech & Language Clinic</a>
+                                <ul className='mt-0'>
+                                    <li>
+                                        <a className='jn-font-pop-blue ms-3 d-block' href="https://education.byu.edu/comd/speech-and-language-clinic/apply" target='_blank' rel='noreferrer'>/apply</a>
+                                    </li>
+                                    <li>
+                                        <a className='jn-font-pop-blue ms-3 d-block' href="https://education.byu.edu/comd/speech-and-language-clinic/about" target='_blank' rel='noreferrer'>/about</a>
+                                    </li>
+                                    <li>
+                                        <a className='jn-font-pop-blue ms-3 d-block' href='https://education.byu.edu/comd/speech-and-language-clinic/resources' target='_blank' rel="noreferrer">/resources</a>
+                                    </li>
+                                </ul>
+                                <h4 className='mb-0'>Description</h4>
+                                <p className='mt-0'>
+                                    I Constructed a series of pages from screenshots provided
+                                    by our UX team. I implemented responsive design techniques
+                                    to ensure an optimal experience on both desktop and mobile
+                                    platforms.
+                                </p>
+                            </div>
+                            <h4 className='mb-2'>Skills and Technologies Used</h4>
+                            <div className='gutter-container flex-gap-xsm'>
+                                <Tag>Bootstrap 5</Tag>
+                                <Tag>CSS</Tag>
+                                <Tag>HTML 5</Tag>
+                                <Tag>Responsive Design</Tag>
                             </div>
                         </div>
                     </div>
-                </main>
-            </>
+                </div>
+            </main>
         )
     }
 }
