0c055eb9b60166c3e3c0106f935df9ad2b681e8a
swalloich <nels.jac42@gmail.com>
2023-12-05 17:48:11 -0700

    Migrated nav-bar to OOP

--- src/components/nav-bar.jsx
+++ src/components/nav-bar.jsx
@@ -1,40 +1,32 @@
-import React from "react";
+import React, {Component} from "react";
+import PropTypes from 'prop-types';
 
 // component and utility function imports
-import { HamburgerButton, toggleHamburgerIcon } from "./hamburger-button";
+import { HamburgerButton } from "./hamburger-button";
 
 // css imports
 import "../styles/components/nav-bar.css";
 
-function NavBar({ navBarId, children }) {
-    function hideNavMenu(target, ms) {
-        if (target) {
-            if (!target.classList.contains("jn-visually-hidden")) {
-                target.classList.add("jn-visually-hidden");
-            }
-            setTimeout(() => {
-                if (!target.classList.contains("jn-hidden")) {
-                    target.classList.add("jn-hidden");
-                }
-            }, ms); 
-        }
+class NavBar extends Component {
+    static propTypes = {
+        children: PropTypes.node,
+        onMenuToggle: PropTypes.func.isRequired,
+        isMenuOpen: PropTypes.bool.isRequired,
     }
 
-    function handleOutsideClick(event) {
-        hideNavMenu(event.target, 200);
-        toggleHamburgerIcon(navBarId);
-    }
+    render () {
+        const { children, onMenuToggle, isMenuOpen } = this.props;
+        const classList = `nav-bar ${isMenuOpen ? '' : 'jn-hidden jn-visually-hidden'}`
 
-    return (
-        <>
-            <nav className="nav-bar jn-hidden jn-visually-hidden" id={navBarId} onClick={handleOutsideClick}>
-                { children }
-            </nav>
-            <HamburgerButton target="main-nav" />
-        </>
-    )
+        return (
+            <>
+                <nav className={classList}>
+                    { children }
+                </nav>
+                <HamburgerButton onNenuClick={onMenuToggle} />
+            </>
+        );
+    }
 }
 
-// TODO: move showNavMenu and closeNavMenu here then export them to be used in other files.
-
-export default NavBar;
+export default NavBar;      
