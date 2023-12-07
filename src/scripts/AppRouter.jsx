d21fb524c2666cd65f481c3326cce93c6c9f2370
swalloich <nels.jac42@gmail.com>
2023-12-06 23:30:31 -0700

    Aliased BrowserRouter as Router on AppRouter

--- src/scripts/AppRouter.jsx
+++ src/scripts/AppRouter.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 // page imports
 import { Home } from '../pages/home';
@@ -8,17 +9,23 @@ import { Contact } from '../pages/contact';
 import { NotFound } from '../pages/not-found';
 
 class AppRouter extends React.Component {
+    static propTypes = {
+        children: PropTypes.node,
+    }
+
     render() {
+        const { children } = this.props;
+
         return (
-            <BrowserRouter>
-                { this.props.children }
+            <Router>
+                { children }
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/projects' element={<Projects />} />
                     <Route path='/contact' element={<Contact />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
-            </BrowserRouter>
+            </Router>
         );
     }
 }
