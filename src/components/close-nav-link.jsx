1a3e9fe7d1f29a5b55e4fcf15c16a2c895968f97
swalloich <nels.jac42@gmail.com>
2023-12-06 23:41:19 -0700

    add props validation to children in close-nav-link

--- src/components/close-nav-link.jsx
+++ src/components/close-nav-link.jsx
@@ -4,14 +4,15 @@ import PropTypes from 'prop-types';
 class CloseNavLink extends React.Component {
     static propTypes = {
         onLinkClicked: PropTypes.func.isRequired,
+        children: PropTypes.node,
     }
 
     render() {
-        const { onLinkClicked } = this.props;
+        const { onLinkClicked, children } = this.props;
 
         return (
             <li className="closeNav" onClick={onLinkClicked}>
-                { this.props.children }
+                { children }
             </li>
         );
     }
