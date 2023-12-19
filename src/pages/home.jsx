32f5f94d446967d293de3d07268daef07f71d8db
swalloich <nels.jac42@gmail.com>
2023-12-18 22:07:08 -0700

    updated landing page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,31 +4,28 @@ import { NavLink } from 'react-router-dom';
 // components
 import FadeIn from "../components/fade-in";
 // resources
-import headshot from "../images/jacob_headshot.jpg"
+import headshot from "../images/jacob_headshot.jpg";
 // style sheets
-import "../styles/utilities/animations.css"
+import "../styles/utilities/animations.css";
+import "../styles/components/dynamic-image.css";
 
 class Home extends React.Component {
     render() {
         return (
-            <main className='d-flex flex-column align-items-center w-100'>
-                <div className="w-100 d-flex justify-content-end py-5 jn-anim-slide-from-right">
-                    <div className="right-edge-card jn-bg-super-light-blue-t">
-                        <img src={headshot} alt="" height={300} width={300} loading='lazy' />
-                        <h1 className='jn-font-xl-md m-0 ms-3 ms-md-5'>Jacob Nelson</h1>
+            <main class="d-flex flex-column-reverse flex-md-row">
+                <div className='dynamic-image-content'>
+                    <div className='container pt-5 px-0 px-md-5'>
+                        <div className='ms-5'>
+                            <h1 className='jn-font-xl ms-0'>Jacob Nelson</h1>
+                            <p>Web Developer</p>
+                        </div>
+                        <div className='mt-5 ms-5'>
+                            <h2 className='jn-font-l'>Need a website?</h2>
+                            <button className='btn btn-primary d-block mt-3'>Click Here</button>
+                        </div>
                     </div>
                 </div>
-                <div className="jn-container">
-                    <FadeIn className='row jn-font-l jn-font-xl-lg' delay={500}>
-                        <div>Need a website?</div>
-                    </FadeIn>
-                    <FadeIn className='row jn-font-l jn-font-xl-lg mt-5' delay={1000}>
-                        <div>Check out my work.</div>
-                    </FadeIn>
-                    <FadeIn className='mt-5' delay={1500}>
-                        <NavLink to="/projects" className="btn btn-primary jn-font-text-color">Click Here</NavLink>
-                    </FadeIn>
-                </div>
+                <div className='dynamic-image-container jn-bg-pop-blue'></div>
             </main>
         )
     }
