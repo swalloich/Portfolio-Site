90929a833ac092fafc87fef90f0f2320cfc63d07
swalloich <nels.jac42@gmail.com>
2025-03-04 17:54:21 -0700

    change App.js indentation to match rest of project

--- src/App.js
+++ src/App.js
@@ -7,24 +7,24 @@ import { PageWrapper } from './components'
 import NotFound from './pages/NotFound'
 
 const links = [
-    { name: 'Home', path: '/' },
-    { name: 'Projects', path: '/projects' },
-    { name: 'Contact', path: '/contact' }
+  { name: 'Home', path: '/' },
+  { name: 'Projects', path: '/projects' },
+  { name: 'Contact', path: '/contact' }
 ]
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<PageWrapper links={links} />}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/projects/*" element={<Projects />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="*" element={<NotFound />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PageWrapper links={links} />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects/*" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
