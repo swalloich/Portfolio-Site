a8b5343af9e0c98ec8bd1b72f396449dacc2b8ba
swalloich <nels.jac42@gmail.com>
2025-03-04 17:51:32 -0700

    Adjust `Image` and `Row`

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 
-export default function Image({ src, alt='', props }) {
+export default function Image({ src, alt='', ...props }) {
   const imageCss = css`
     width: 100%;
     height: auto;
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,7 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
+import PropTypes from 'prop-types'
 
-export default function Row({ children, justify = "flex-start", align = "start" }) {
+export default function Row({ align = "start", children, justify = "flex-start", ...props }) {
   const rowCss = css`
     display: flex;
     flex-direction: row;
@@ -10,5 +11,15 @@ export default function Row({ children, justify = "flex-start", align = "start" 
     align-items: ${align};
   `
 
-  return <div css={rowCss}>{children}</div>
+  return (
+    <div css={rowCss} {...props}>
+      {children}
+    </div>
+  )
+}
+
+Row.propTypes = {
+  align: PropTypes.oneOf(["start", "center", "end", "stretch"]),
+  children: PropTypes.node,
+  justify: PropTypes.oneOf(["start", "end", "flex-start", "flex-end", "center", "left", "right", "normal", "space-between", "space-around", "space-evenly", "stretch", "safe", "unsafe"]),
 }
