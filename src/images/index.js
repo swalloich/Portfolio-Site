8e9c82e7fff0f3654c092c92278dff03b852cbe6
swalloich <nels.jac42@gmail.com>
2025-03-20 14:36:09 -0600

    did something wrong

--- src/images/index.js
+++ src/images/index.js
@@ -1,5 +1,5 @@
 import creepyHandshake from './creepy-handshake.webp'
 import jacobHeadshot from './jacob-headshot.webp'
-import logo from './logo.pwebp'
+import logo from './logo.webp'
 
 export { creepyHandshake, jacobHeadshot, logo }
